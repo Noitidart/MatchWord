4bbe0e10d9cce4498ab0dee0d7eec0dfa1f9f9d3
yajd <yasir.ali@jdsu.com>
2014-06-24 11:57:33 -0700

    Update bootstrap.js
    got the css applying to profilist buttons

--- bootstrap.js
+++ bootstrap.js
@@ -17,7 +17,8 @@ XPCOMUtils.defineLazyGetter(myServices, 'sss', function(){ return Cc['@mozilla.o
 function startup(aData, aReason) {
 	self.aData = aData;
 	console.log('aData',aData);
-	var css = '.findbar-container { -moz-binding:url("' + self.path.chrome + 'findbar.xml#matchword") }';
+	var css = '';
+	css += '.PanelUI-profilist { -moz-binding:url("' + self.path.chrome + 'findbar.xml#matchword") }';
 	var cssEnc = encodeURIComponent(css);
 	var newURIParam = {
 		aURL: 'data:text/css,' + cssEnc,
